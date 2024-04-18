4360a5e507d15b3fc01b7045a44da3124ebd61c4
MathiasLinde <113913029+MathiasLinde@users.noreply.github.com>
2024-04-18 22:55:27 +0200

    added POST

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,4 @@
-from flask  import Flask 
+from flask  import Flask, request, jsonify 
 from jsonreader import read_json_from_file  
 import datetime
 
@@ -12,6 +12,19 @@ def get_products():
     json_data = read_json_from_file(file_path)
     
     return json_data
+
+@app.route('/checkout_data/', methods = ['POST'])
+def checkout_data():
+    data = request.get_json()
+
+    print("Recieved data:", data)
     
+    // implement prepared statements to add checkout_data to database
+
+    return 200
+    
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
